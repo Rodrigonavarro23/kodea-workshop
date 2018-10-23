54cc0b328bee0bdc04c9c07e7d5290292177b359
R0n0066 <rodrigo.navarro@walmart.com>
2018-10-22 21:17:43 -0300

    First commit

---
+++ src/components/dashboard/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Row, Col } from "reactstrap";
+
+import "./index.css";
+
+const Stage = props => (
+  <Col xs="12" md={props.size}>
+    <div className="ws-dashboard__stage-title">{props.title}</div>
+    <div className="ws-dashboard__stage-cards">Card list</div>
+  </Col>
+);
+
+const Dashboard = props => (
+  <Row>
+    {props.stages.map(stage => (
+      <Stage {...stage} size={12 / props.stages.length} />
+    ))}
+  </Row>
+);
+
+export default Dashboard;
